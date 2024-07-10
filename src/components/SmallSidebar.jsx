2fcabc76295a3e583659e1a9d1ae94b084946be2
Maggie Gong <maggiegongsh@gmail.com>
2024-07-10 14:37:04 -0700

    update dashboard
    - setup dashboard structure
    - set up sidebar structure
    - add icons to navbar and small sidebar
    - add links component for sidebar navigation

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,9 +1,41 @@
+import { FaTimes } from "react-icons/fa";
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { useDashboardContext } from "../pages/DashboardLayout";
+import Logo from "./Logo";
+import Links from "../utils/Links";
+import { NavLink } from "react-router-dom";
 
 const SmallSidebar = () => {
 	const data = useDashboardContext();
-	console.log(data);
-	return <Wrapper>SmallSidebar</Wrapper>;
+
+	return (
+		<Wrapper>
+			<div className="sidebar-container show-sidebar">
+				<div className="content">
+					<button type="button" className="close-btn">
+						<FaTimes />
+					</button>
+					<header>
+						<Logo />
+					</header>
+					<div className="nav-links">
+						{Links.map((link) => {
+							const { text, path, icon } = link;
+							return (
+								<NavLink
+									to={path}
+									key={text}
+									className={"nav-link"}
+								>
+									<span className="icon">{icon}</span>
+									{text}
+								</NavLink>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
 export default SmallSidebar;
