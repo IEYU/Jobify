17a262e6d669494f54636eb6b7fb761df10aa5c6
Maggie Gong <maggiegongsh@gmail.com>
2024-07-09 21:00:49 -0700

    Update Error.jsx
    - styling wrappers
    - error page image and text
    - error page redirection

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,14 +1,22 @@
 import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from "../assets/images/not-found.svg";
 
 const Error = () => {
-  const error = useRouteError();
-  console.log(error);
+	const error = useRouteError();
+	console.log(error);
 
-  return (
-    <div>
-      <h1>Error</h1>
-      <Link to="/">Back</Link>
-    </div>
-  );
+	if (error.status === 404) {
+		return (
+			<Wrapper>
+				<div>
+					<img src={img} alt="not found" />
+					<h3>Ohh! Page not found</h3>
+					<p>We can't find the page you are looking for</p>
+					<Link to="/dashboard">Go Back</Link>
+				</div>
+			</Wrapper>
+		);
+	}
 };
 export default Error;
