a9b01450ef3b2cef4a217fce82d3b652bb4d8ac0
Maggie Gong <maggiegongsh@gmail.com>
2024-07-10 16:15:33 -0700

    updated logout container component
    - toggle button to show and hide logout option

---
+++ src/components/LogoutContainer.jsx
@@ -0,0 +1,37 @@
+import { useDashboardContext } from "../pages/DashboardLayout";
+import Wrapper from "../assets/wrappers/LogoutContainer";
+import { FaCaretDown, FaUserCircle } from "react-icons/fa";
+import { useState } from "react";
+
+const LogoutContainer = () => {
+	const [showLogout, setShowLogout] = useState(false); //useState hook returns a pair: the current state value and an update function
+	const { user, logoutUser } = useDashboardContext(); //useContext hook is used to access the context value, which can be any value passed to the context provider
+
+	return (
+		<Wrapper>
+			<button
+				type="button"
+				className="btn logout-btn"
+				onClick={() => setShowLogout(!showLogout)}
+			>
+				<FaUserCircle />
+				{user?.name}
+				<FaCaretDown />
+				<div
+					className={
+						showLogout ? "dropdown show-dropdown" : "dropdown"
+					}
+				>
+					<button
+						type="button"
+						className="dropdown-btn"
+						onClick={logoutUser}
+					>
+						logout
+					</button>
+				</div>
+			</button>
+		</Wrapper>
+	);
+};
+export default LogoutContainer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import Wrapper from "../assets/wrappers/Navbar";
 import { FaAlignLeft } from "react-icons/fa";
 import Logo from "./Logo";
 import { useDashboardContext } from "../pages/DashboardLayout";
+import LogoutContainer from "./LogoutContainer";
 
 const Navbar = () => {
 	const { toggleSidebar } = useDashboardContext();
@@ -20,8 +21,10 @@ const Navbar = () => {
 					<Logo />
 					<h4 className="logo-text">dashboard</h4>
 				</div>
-				<div className="btn-container">toggle/logout</div>
-			</div>{" "}
+				<div className="btn-container">
+					<LogoutContainer />
+				</div>
+			</div>
 		</Wrapper>
 	);
 };
