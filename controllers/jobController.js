80b1500778c738bf084ea9d67b8dffe2174e29a7
Maggie Gong <maggiegongsh@gmail.com>
2024-07-26 23:41:24 -0700

    setup sending back cookies

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -6,6 +6,7 @@ import { NotFoundError } from "../errors/customErrors.js";
 
 // get all jobs
 export const getAllJobs = async (req, res)=>{
+    console.log(req)
     const jobs = await Job.find({})
     res.status(StatusCodes.OK).json({ jobs });
 }
